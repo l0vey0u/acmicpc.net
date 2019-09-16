399cac0f54ae97d3eb7c68bec77a8ad8e2537075
Deokho Jeong <duckhoim@kyonggi.ac.kr>
2019-09-16 22:21:02 +0900

    반례를 찾을 수 없어서 결국 또..
    내가 이런 코드를 생각하지 못한 이유는 무엇일까?
    일단 R3에 대해 제대로 "표현" 해내지 못했다..
    R3을 벗어나려고만 했을 뿐
    1,2,3,4 계단이 있을때
    1계단을 밟았을 때는 3,4 를 밟을 수 있지만
    2계단을 밟았을 때는 4만 밟을 수 있다..
    
    아직도 DP에는 덜 익숙한 것 같다.
    더 풀어야 겠다.

--- 2579.py
+++ 2579.py
@@ -1,18 +1,11 @@
 T = int(input())
-scr = []
+p = []
 for _ in range(T):
-    scr.append(int(input()))
+    p.append(int(input()))
 dp = [ 0 for _ in range(T+1) ]
-for n in range(1, T+1):
-    dp[n] = scr[n-1]
-    if n == 1:
-        continue
-    if n == 2:
-        if T != 3:
-            dp[n] += scr[n-2]
-            continue
-    if scr[n-2] > scr[n-3]:
-        dp[n] += dp[n-1]
-    else:
-        dp[n] += dp[n-2]
+dp[1] = p[0]
+dp[2] = p[0] + p[1]
+dp[3] = max(p[0], p[1]) + p[2]
+for n in range(3, T+1):
+    dp[n] = max(dp[n-3] + p[n-2] + p[n-1], dp[n-2] + p[n-1])
 print(dp[T])
